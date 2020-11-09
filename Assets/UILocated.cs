7012bdc756223c700c08e7ac487f88d8cfaef92e
Foxtill <53660090+Foxtill@users.noreply.github.com>
2020-11-09 03:20:05 +0300

    Add files via upload

---
+++ Assets/UILocated.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UILocated : MonoBehaviour {
+
+	private int xRes;
+	private int yRes;
+	Canvas canvas;
+	public Button[] Enter;
+	void Start () {
+		Resolution[] screenRes = Screen.resolutions;
+		foreach(var res in screenRes)
+        {
+			xRes = res.height;
+			yRes = res.width;
+        }
+		foreach (var but in Enter)
+		{
+			switch (but.name)
+			{
+				case "BwithG":
+					break;
+				case "BwithFB":
+					break;
+				case "Enter":
+					break;
+			}
+		}
+		//gameObject.transform 
+		//canvas.scaleFactor = new 
+
+	}
+}
